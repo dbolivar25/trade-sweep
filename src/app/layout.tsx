99b35a2e6ac586f70d2ce5ddb956c95853b426df
dbolivar25 <danielbolivarflorez@gmail.com>
2025-04-18 14:52:19 -0500

    add clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { AppSidebar } from "@/components/layout/app-sidebar";
 import { CommandMenu } from "@/components/layout/command-bar";
 import { ThemeProvider } from "@/components/layout/theme-provider";
 import { ThemeToggle } from "@/components/layout/theme-toggle";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,30 +53,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <SidebarProvider>
-            <AppSidebar />
-            <div className="flex-1 w-full">
-              <div className="sticky top-0 z-10 p-4 bg-white dark:bg-stone-950 flex items-center justify-between">
-                <SidebarTrigger className="mt-2" />
-                <ThemeToggle />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SidebarProvider>
+              <AppSidebar />
+              <div className="flex-1 w-full">
+                <div className="sticky top-0 z-10 p-4 bg-white dark:bg-stone-950 flex items-center justify-between">
+                  <SidebarTrigger className="mt-2" />
+                  <ThemeToggle />
+                </div>
+                <div className="w-full">{children}</div>
+                <CommandMenu />
+                <Toaster />
               </div>
-              <div className="w-full">{children}</div>
-              <CommandMenu />
-              <Toaster />
-            </div>
-          </SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+            </SidebarProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
