7bb5c281ae529ecd2c5a39bd22aeaf08722887db
dbolivar25 <danielbolivarflorez@gmail.com>
2025-04-18 16:55:09 -0500

    add react query

---
+++ src/components/providers/react-query-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useState, ReactNode } from "react";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: 5 * 60 * 1000, // 5 minutes
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
