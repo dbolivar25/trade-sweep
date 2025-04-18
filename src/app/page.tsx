bd31a20f1d3bbb4b61ffa5188d5756fbc61da786
dbolivar25 <danielbolivarflorez@gmail.com>
2025-04-18 14:52:26 -0500

    add clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
+"use client";
+
 import RecentTradesCard from "@/components/dashboard/recent-trades-card";
 import WatchlistCard from "@/components/dashboard/watchlist-card";
 import WelcomeCard from "@/components/dashboard/welcome-card";
+import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, user } = useUser();
   return (
     <div className="w-full px-4 pb-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-screen-2xl mx-auto">
         {/* Left Column - Welcome Card and Watchlist */}
         <div className="space-y-6 w-full">
-          <WelcomeCard />
-          <WatchlistCard />
+          <WelcomeCard
+            isLoaded={isLoaded}
+            isSignedIn={!!isSignedIn}
+            user={user}
+          />
+          <WatchlistCard
+            isLoaded={isLoaded}
+            isSignedIn={!!isSignedIn}
+            user={user}
+          />
         </div>
 
         {/* Right Column - Recent Trades */}
         <div className="w-full">
-          <RecentTradesCard />
+          <RecentTradesCard
+            isLoaded={isLoaded}
+            isSignedIn={!!isSignedIn}
+            user={user}
+          />
         </div>
       </div>
     </div>
